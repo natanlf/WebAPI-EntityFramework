6bde15d06d29362401cfd68d86856ef907e8684c
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2019-04-11 12:11:41 -0300

    Adicionando o SwitchContext

---
+++ Switch.Infra.Data/Context/SwitchContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Switch.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Switch.Infra.Data.Context
+{   
+    //A classe Context herda de DBContext
+    public class SwitchContext : DbContext
+    {
+        //Usuarios é o nome da tabela que será criada
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        //Consttrutor e acesso o construtor da classe pai e passo options
+        public SwitchContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
